e2acdd5a173c1466f9418febc3dfc4380f54b174
Tessa <tessa@nrdgrlz.org>
2019-06-03 10:26:37 +0200

    :sparkles: :art: UUID nach Copied
    Nach dem Kopieren wird die UUID des neu erzeugten Drafts nach copied
    kopiert, ausserdem stilistische Anpassungen (single quotes, vier Spaces
    Einrückungen).

--- weekly_taskpaper.js
+++ weekly_taskpaper.js
@@ -1,35 +1,43 @@
-const toDoHeadStart = "Recurring Heading, calender week gets appended"
-const permanentTags = ["recurring", "tags"];
+const toDoHeadStart = 'Recurring Heading, calender week gets appended';
+const permanentTags = ['recurring', 'tags'];
 
 // zuletzt erzeugtes isc-todo auswählen
 const [lastWeek] = Draft.query(
-  toDoHeadStart,
-  "inbox",
-  permanentTags,
-  [],
-  "created",
-  true
+    toDoHeadStart,
+    'inbox',
+    permanentTags,
+    [],
+    'created',
+    true,
 );
-const [, ...content] = lastWeek.content.split("\n");
+const [, ...content] = lastWeek.content.split('\n');
 
 // neues dokument anlegen
 const newWeek = Draft.create();
-newWeek.languageGrammar = "Taskpaper";
+newWeek.languageGrammar = 'Taskpaper';
 const timestamp = new Date();
 // %V ist die ISO-Woche mit führender 0, %Y das (vierstellige) Jahr
 [
-  ...permanentTags,
-  // additional tags: kw + calendar week, year
-  "kw" + strftime(timestamp, "%V"),
-  strftime(timestamp, "%Y")
+    ...permanentTags,
+    'kw' + strftime(timestamp, '%V'),
+    strftime(timestamp, '%Y'),
 ].forEach(tag => newWeek.addTag(tag));
 // den alten Content in Zeilen zerlegen, diese durchgehen
-const heading = [toDoHeadStart, strftime(timestamp, "%V/%Y")].join(" ");
+const heading = [toDoHeadStart, strftime(timestamp, '%V/%Y')].join(' ');
 // erledigte tasks löschen (momentan bleiben die kommentare und projekte noch erhalten...)
-const newContent = content.filter(line => !line.includes("@done"));
-newWeek.content = [heading, ...newContent].join("\n");
+const newContent = content.filter(line => !line.includes('@done'));
+newWeek.content = [heading, ...newContent].join('\n');
 newWeek.update();
 
 // altes dokument ins archiv
 lastWeek.isArchived = true;
 lastWeek.update();
+
+// jetzt noch die in copied speichern und die neu angelegte liste anzeigen
+uuid = newWeek.uuid;
+app.openURL(
+    'copied://x-callback-url/save?text=' +
+        uuid +
+        '&x-success=drafts5://open?uuid=' +
+        uuid,
+);
